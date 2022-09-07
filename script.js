15bf6859e3bdef711b9f906f438d8397083fc0d9
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-09-07 09:03:49 +0600

    do some work

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 // get text value from id
 const getInput = (id) => {
     const inputField = document.getElementById(id)
-    return inputField.value
+    const inputText = inputField.value
+    inputField.value = ``
+    return inputText
+}
+// get from local storage function
+const
+// set to local storage function
+// const setLocalStorage = ()
+// display todo value
+const display = () => {
+    const toDoContainer = document.getElementById('todo-list')
+    const toDoValue = getInput('todo-text')
+    const li = document.createElement('li')
+    li.innerHTML = `
+    <div class="flex justify-between my-2">
+      ${toDoValue}
+      <button class="bg-green-400 px-2 rounded-lg text-white">Delete</button>
+    </div>
+     
+    `
+    toDoContainer.appendChild(li)
 }
 
+// plus btn event listener
+const addItem = () => {
+    display()
+    
+}
